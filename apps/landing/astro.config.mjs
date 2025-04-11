9a8901008e2bc547976ca60f52f066f16c5082df
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-04-11 00:03:13 -0700

    build: set linting to app only

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: "https://frame-rate.io",
-});
+// https://astro.build/config
+export default defineConfig({});
